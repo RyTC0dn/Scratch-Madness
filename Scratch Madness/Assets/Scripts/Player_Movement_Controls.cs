fb3911918f800601d71e3816edab862aacc5a08c
COdinRyT <rthompso96@gmail.com>
2026-05-06 12:58:33 -0400

    Aiming weapon controls
    Read the Vector2 input value to rotate the weapon around the player with the mouse or right stick on the controller.
    
    Also preparing a projectile game object to propel at targets, and eventually damage them.

--- Scratch Madness/Assets/Scripts/Player_Movement_Controls.cs
+++ Scratch Madness/Assets/Scripts/Player_Movement_Controls.cs
@@ -6,6 +6,7 @@ public class Player_Movement_Controls : MonoBehaviour
 {
     [Header("Player Component Settings")]
     private Rigidbody2D rb;
+    public GameObject rotationPoint;
 
     public float moveSpeed = 5f;
 
@@ -24,12 +25,25 @@ public class Player_Movement_Controls : MonoBehaviour
 
     #region Player Input Methods
 
+    //This method is to define the movement inputs
+    //with keyboard or left joystick
     public void Move(InputAction.CallbackContext context)
     {
         // Read the movement input values from the context and
         // store them in the moveInputX and moveInputY variables
         moveInputX = context.ReadValue<Vector2>().x;
         moveInputY = context.ReadValue<Vector2>().y;
+    }
+
+    //This method is to define the inputs for being able to aim
+    //with mouse or right joystick
+    public void Aim(InputAction.CallbackContext context)
+    {
+        Vector2 aimInput = context.ReadValue<Vector2>();
+        // Calculate the angle between the player and the aim input
+        float angle = Mathf.Atan2(aimInput.y, aimInput.x) * Mathf.Rad2Deg;
+        // Rotate the weapon in the direction of the aim input
+        rotationPoint.transform.rotation = Quaternion.Euler(0f, 0f, angle);
     }
 
     #endregion Player Input Methods
